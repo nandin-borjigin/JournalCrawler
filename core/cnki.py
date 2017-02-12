0a65ab7cb0715b6fc644f75e48210f5273ca2107
Nandiin <nandiin@163.com>
2017-02-12 16:22:31 +0000

    CNKI class

---
+++ core/cnki.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+import random
+
+class CNKI(object):
+    def __init__(self):
+        self.search_url = 'http://navi.cnki.net/KNavi/Common/SearchAll'
+        self.search_method = 'POST'
+        self.search_headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+    def search_body(self, journal, page):
+        prefix = '{"StateID":"","Platfrom":"","QueryTime":"","Account":"knavi","ClientToken":"","Language":"","CNode":{"PCode":"SCDB","SMode":"","OperateT":""},"QNode":{"SelectT":"","Select_Fields":"","S_DBCodes":"","QGroup":[{"Key":"subject","Logic":1,"Items":[],"ChildItems":[{"Key":"txt","Logic":1,"Items":[{"Key":"txt_1","Title":"","Logic":1,"Name":"LY","Operate":"%","Value":"'
+        postfix = '","ExtendType":0,"ExtendValue":"","Value2":""}],"ChildItems":[]}]}],"OrderBy":"","GroupBy":"","Additon":""}}'
+        return {
+            'SearchStateJson': prefix + journal.name + postfix,
+            'displaymode': 1,
+            'pageindex': page,
+            'pagecount': 10,
+            'random': random.random()
+        }
+
+    
---
+++ tests/test_cnki.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+from core.cnki import CNKI
+from core.journal import Journal
+from fixtures.common import *
+
+@pytest.fixture
+def cnki():
+    return CNKI()
+
+def test_search_url(cnki):
+    assert cnki.search_url == 'http://navi.cnki.net/KNavi/Common/SearchAll'
+
+def test_search_method(cnki):
+    assert cnki.search_method == 'POST'
+
+def test_search_headers(cnki):
+    assert cnki.search_headers == {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+@pytest.fixture( params = journal_names )
+def journal(request):
+    return Journal(request.param)
+
+def test_search_body(cnki, journal, page):
+    prefix = '{"StateID":"","Platfrom":"","QueryTime":"","Account":"knavi","ClientToken":"","Language":"","CNode":{"PCode":"SCDB","SMode":"","OperateT":""},"QNode":{"SelectT":"","Select_Fields":"","S_DBCodes":"","QGroup":[{"Key":"subject","Logic":1,"Items":[],"ChildItems":[{"Key":"txt","Logic":1,"Items":[{"Key":"txt_1","Title":"","Logic":1,"Name":"LY","Operate":"%","Value":"'
+    postfix = '","ExtendType":0,"ExtendValue":"","Value2":""}],"ChildItems":[]}]}],"OrderBy":"","GroupBy":"","Additon":""}}'
+    search_state_json =  prefix + journal.name + postfix
+    def body():
+        return    
+    search_body = cnki.search_body(journal, page)
+    assert 0 <= search_body['random'] < 1
+    del search_body['random']
+    assert search_body == {
+        'SearchStateJson': search_state_json,
+        'displaymode': 1,
+        'pageindex': page,
+        'pagecount': 10
+    }
+
